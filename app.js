bd80597f353509b8da81980ee266cf3009534592
Mahmood <mhaq@hotmail.com>
2020-12-08 23:53:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Jenkins New Pipeline Test!'));
+app.get('/', (req, res) => res.send('Rayan Pipeline Test!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
